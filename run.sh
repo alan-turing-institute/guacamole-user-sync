988017f65c242b7a20d9f0ca4b72801edb031d52
James Robinson <james.em.robinson@gmail.com>
2023-06-28 11:56:48 +0100

    :loud_sound: Simpler logging output

--- run.sh
+++ run.sh
@@ -1,15 +1,15 @@
 #! /bin/bash
 
 # Expand mustache template
-echo "$(date -I"seconds") Expanding mustache templates..."
+echo "$(date -Is) Expanding mustache templates..."
 /app/scripts/expand_pg_ldap_sync.rb > /app/resources/pg-ldap-sync.yaml
 chmod 0600 /app/resources/pg-ldap-sync.yaml
 /app/scripts/expand_psql.rb > /app/scripts/psql.sh
 chmod 0700 /app/scripts/psql.sh
 
 # Run LDAP synchronisation
-echo "$(date -I"seconds") Running LDAP synchronisation..."
+echo "$(date -Is) Running LDAP synchronisation..."
 /usr/local/bin/pg_ldap_sync -vvv -c /app/resources/pg-ldap-sync.yaml 2>&1
-echo "$(date -I"seconds") Updating database..."
+echo "$(date -Is) Updating database..."
 /app/scripts/psql.sh
-echo "$(date -I"seconds") Finished database synchronisation"
+echo "$(date -Is) Finished database synchronisation"
