5eca6391ef6de7273f40f4cac278cf579fc4aea2
James Robinson <james.em.robinson@gmail.com>
2023-06-28 16:31:00 +0100

    :sparkles: Add database initialisation to synchronisation setup

--- templates/psql.mustache.sh
+++ templates/psql.mustache.sh
@@ -1,5 +1,8 @@
 #! /bin/bash
 
+# Run database initialisation
+psql "host={{POSTGRES_HOST}} port={{POSTGRES_PORT}} dbname={{POSTGRES_DB_NAME}} user={{POSTGRES_USERNAME}} password={{POSTGRES_PASSWORD}} sslmode=require" -f /app/resources/init_db.sql
+
 # Run LDAP synchronisation
 psql "host={{POSTGRES_HOST}} port={{POSTGRES_PORT}} dbname={{POSTGRES_DB_NAME}} user={{POSTGRES_USERNAME}} password={{POSTGRES_PASSWORD}} sslmode=require" -f /app/resources/update_users.sql
 
