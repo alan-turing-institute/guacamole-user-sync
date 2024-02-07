f543feeba8cc2735ac33571e52688f3d8d498d30
James Robinson <james.em.robinson@gmail.com>
2024-02-07 21:05:16 +0000

    :wrench: Update POSTGRES_* variables to POSTGRESQL_*

--- templates/psql.mustache.sh
+++ templates/psql.mustache.sh
@@ -1,8 +1,7 @@
 #! /bin/bash
 
 # Run database initialisation so that the desired tables exist
-psql "host={{POSTGRES_HOST}} port={{POSTGRES_PORT}} dbname={{POSTGRES_DB_NAME}} user={{POSTGRES_USERNAME}} password={{POSTGRES_PASSWORD}} sslmode=require" -f /app/resources/init_db.sql
+psql "host={{POSTGRESQL_HOST}} port={{POSTGRESQL_PORT}} dbname={{POSTGRESQL_DB_NAME}} user={{POSTGRESQL_USERNAME}} password={{POSTGRESQL_PASSWORD}} sslmode=require" -f /app/resources/init_db.sql
 
 # Run LDAP synchronisation
-psql "host={{POSTGRES_HOST}} port={{POSTGRES_PORT}} dbname={{POSTGRES_DB_NAME}} user={{POSTGRES_USERNAME}} password={{POSTGRES_PASSWORD}} sslmode=require" -f /app/resources/update_users.sql
-
+psql "host={{POSTGRESQL_HOST}} port={{POSTGRESQL_PORT}} dbname={{POSTGRESQL_DB_NAME}} user={{POSTGRESQL_USERNAME}} password={{POSTGRESQL_PASSWORD}} sslmode=require" -f /app/resources/update_users.sql
