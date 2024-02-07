ce30688e88c0445deaeb80dc207f6252b693b9f7
James Robinson <james.em.robinson@gmail.com>
2024-02-07 21:21:06 +0000

    :bug: Escape all user-variables in mustache templates

--- templates/psql.mustache.sh
+++ templates/psql.mustache.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 # Run database initialisation so that the desired tables exist
-psql "host={{POSTGRESQL_HOST}} port={{POSTGRESQL_PORT}} dbname={{POSTGRESQL_DB_NAME}} user={{POSTGRESQL_USERNAME}} password={{POSTGRESQL_PASSWORD}} sslmode=require" -f /app/resources/init_db.sql
+psql "host={{{POSTGRESQL_HOST}}} port={{{POSTGRESQL_PORT}}} dbname={{{POSTGRESQL_DB_NAME}}} user={{{POSTGRESQL_USERNAME}}} password={{{POSTGRESQL_PASSWORD}}} sslmode=require" -f /app/resources/init_db.sql
 
 # Run LDAP synchronisation
-psql "host={{POSTGRESQL_HOST}} port={{POSTGRESQL_PORT}} dbname={{POSTGRESQL_DB_NAME}} user={{POSTGRESQL_USERNAME}} password={{POSTGRESQL_PASSWORD}} sslmode=require" -f /app/resources/update_users.sql
+psql "host={{{POSTGRESQL_HOST}}} port={{{POSTGRESQL_PORT}}} dbname={{{POSTGRESQL_DB_NAME}}} user={{{POSTGRESQL_USERNAME}}} password={{{POSTGRESQL_PASSWORD}}} sslmode=require" -f /app/resources/update_users.sql
